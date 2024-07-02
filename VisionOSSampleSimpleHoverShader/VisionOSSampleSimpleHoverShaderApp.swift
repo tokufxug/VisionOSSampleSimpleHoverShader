4447aca06ad1a112357867bf883da3e3725d8bbe
tokufxug <tokufxug@gmail.com>
2024-07-02 23:53:43 +0900

    Initial Commit

---
+++ VisionOSSampleSimpleHoverShader/VisionOSSampleSimpleHoverShaderApp.swift
@@ -0,0 +1,18 @@
+//
+//  VisionOSSampleSimpleHoverShaderApp.swift
+//  VisionOSSampleSimpleHoverShader
+//
+//  Created by Sadao Tokuyama on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct VisionOSSampleSimpleHoverShaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowStyle(.volumetric)
+    }
+}
---
+++ VisionOSSampleSimpleHoverShaderTests/VisionOSSampleSimpleHoverShaderTests.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOSSampleSimpleHoverShaderTests.swift
+//  VisionOSSampleSimpleHoverShaderTests
+//
+//  Created by Sadao Tokuyama on 7/2/24.
+//
+
+import Testing
+@testable import VisionOSSampleSimpleHoverShader
+
+struct VisionOSSampleSimpleHoverShaderTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
